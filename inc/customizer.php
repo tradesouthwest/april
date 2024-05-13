b4cadef9f2db06270bdc827cb0c4e3ce9400c58b
Larry <tradesouthwest@outlook.com>
2024-05-13 08:47:54 -0700

    Update customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -29,55 +29,78 @@ function april_sanitize_integer( $input ) {
     }
 } 
 
+/**
+ * Set default values in an arry
+ * $hcdf  = get_template_directory_uri() . '/rels/swimmers-wide-default.jpg';
+ * $hcdg  = get_template_directory_uri() . '/rels/1x1.png';
+ * $hsdf  = get_stylesheet_directory_uri() . '/';
+ */
+function april_theme_defaults( $args ){
+	$defaults = array(
+		'fntfamily'  => 'sans-serif',
+		'herouri'    => get_template_directory_uri() . '/rels/1x1.png',
+		'herobksize' => 'cover',
+		'siteta'     => '#dddddd',
+		'ctaposit'   => '51',
+		'rlp'        => '10',
+		'rtp'        => '10',
+		'maxw'       => '1200',
+		'tposit'     => 'left'
+	);
+	$args = wp_parse_args( $args, $defaults );
+
+	return( $args );
+}
+
 /** A1
  * CUSTOM FONT OUTPUT, CSS
  * The @font-face rule should be added to the stylesheet before any styles. (priority 2)
  * @uses background-image as linear gradient meerly remove any input background image.
  * @since 1.0.0
 */
-function april_theme_customizer_css() 
-{   
-    $fnt = $font = ''; $gen_colr = '#494949'; 
-   
-    $hcdf  = get_template_directory_uri() . '/rels/swimmers-wide-default.jpg';
-	$hcdg  = get_template_directory_uri() . '/rels/1x1.png';
-    $hsdf  = get_stylesheet_directory_uri() . '/';
+function april_theme_customizer_css() {
     
     if( get_theme_mods() ) : 
-	$fntfamily  = ( empty( get_theme_mod( 'april_fontfamily' ) ) ) ? 'sans-serif' 
-				: wp_strip_all_tags( get_theme_mod( 'april_fontfamily' ) );
-    // background image
-	$herouri  = ( empty( get_theme_mod( 'april_herocall' ) ) ) ? esc_url( $hcdg )  
-					   : get_theme_mod( 'april_herocall' );
+	$fntfamily  = ( empty( get_theme_mod( 'april_fontfamily' ) ) ) ? april_theme_defaults( esc_attr( $args['fntfamily'])) 
+		      : wp_strip_all_tags( get_theme_mod( 'april_fontfamily' ) );
+        // background image
+	$herouri    = ( empty( get_theme_mod( 'april_herocall' ) ) ) ? april_theme_defaults( esc_url( $args['herouri']) ) 
+		      : get_theme_mod( 'april_herocall' );
 	// hero background size
-	$herobksize  = ( empty( get_theme_mod( 'april_herobksize' ) ) ) 
-	            ? 'cover' : get_theme_mod( 'april_herobksize' );
-	
+	$herobksize = ( empty( get_theme_mod( 'april_herobksize' ) ) ) ? april_theme_defaults( esc_attr( $args['fntfamily'])) 
+		      : get_theme_mod( 'april_herobksize' );
 	// site title color
-	$siteta     = ( empty( get_theme_mod( 'april_siteta' ) ) ) 
-	            ? '#ffffff' : get_theme_mod( 'april_siteta' );
+	$siteta     = ( empty( get_theme_mod( 'april_siteta' ) ) ) ? april_theme_defaults( esc_attr( $args['siteta'])) 
+		      : get_theme_mod( 'april_siteta' );
 	// april_ctaposit
-	$ctaposit   = ( empty( get_theme_mod( 'april_ctaposit' ) ) ) 
-	            ? '51' : get_theme_mod( 'april_ctaposit' );
-    $rlp        = ( empty( get_theme_mod( 'april_rlposit' ) ) ) ? '10' 
-                         : get_theme_mod( 'april_rlposit' );
-    $rtp        = ( empty( get_theme_mod( 'april_rtposit' ) ) ) ? '10' 
-                         : get_theme_mod( 'april_rtposit' );
-	$maxw       = ( empty( get_theme_mod( 'april_maxwidth' ) ) ) ? '1200' 
-                         : get_theme_mod( 'april_maxwidth' );        
-    endif;
-	/* use above set values into inline styles */
-    $cssstyles = 
-	'body, button, input, select, textarea, p{ font-family: '. esc_attr( $fntfamily ) .';}
-	.site-title, .site-description{ position: relative; left: '. esc_attr( $rlp ) .'px; top: '. esc_attr( $rtp ) .'px;}
-	.site-title a, site-description{ color: ' . esc_attr( $siteta ) .';}
-	.current_page_item a, .current_page_ancestor a{background: rgba(252,252,252, .8);}
-    .herocall{background-image: url('. esc_url( $herouri ) .') ; background-size: '.esc_attr($herobksize).';}
-	.ctasection{margin-left: '.esc_attr( $ctaposit ) .'%}
-	.page article.page, .single article.post{max-width: '. esc_attr( $maxw ) .'px;margin: 0 auto;}';
-    wp_register_style( 'april-inline-customizer', true );
-	wp_enqueue_style( 'april-inline-customizer' );
-	wp_add_inline_style( 'april-inline-customizer', $cssstyles );
+	$ctaposit   = ( empty( get_theme_mod( 'april_ctaposit' ) ) ) ? april_theme_defaults( esc_attr( $args['ctaposit'] )) 
+		      : get_theme_mod( 'april_ctaposit' );
+        $rlp        = ( empty( get_theme_mod( 'april_rlposit' ) ) ) ? april_theme_defaults( esc_attr( $args['rlp'])) 
+                      : get_theme_mod( 'april_rlposit' );
+        $rtp        = ( empty( get_theme_mod( 'april_rtposit' ) ) ) ? april_theme_defaults( esc_attr( $args['rtp'])) 
+                      : get_theme_mod( 'april_rtposit' );
+	$tposit     = ( empty( get_theme_mod( 'april_tposit' ) ) ) ? april_theme_defaults( esc_attr( $args['tposit'])) 
+                      : get_theme_mod( 'april_tposit' );
+	// max width
+	$maxw       = ( empty( get_theme_mod( 'april_maxwidth' ) ) ) ? april_theme_defaults( esc_attr( $args['maxw']))
+                      : get_theme_mod( 'april_maxwidth' );        
+
+	    /* use above set values into inline styles */
+	    $cssstyles = 
+		'body, button, input, select, textarea, p{ font-family: '. esc_attr( $fntfamily ) .';}
+		.site-title, .site-description{ position: relative; left: '. esc_attr( $rlp ) .'px; top: '. esc_attr( $rtp ) .'px;}
+		.site-title a, site-description{ color: ' . esc_attr( $siteta ) .';}
+		.current_page_item a, .current_page_ancestor a{background: rgba(252,252,252, .8);}
+	        .herocall{background-image: url('. esc_url( $herouri ) .') ; background-size: '.esc_attr($herobksize).';}
+		.ctasection{margin-left: '.esc_attr( $ctaposit ) .'%}.site-branding{ text-align: '. esc_attr( $tposit ) .';}
+		.page article.page, .single article.post{max-width: '. esc_attr( $maxw ) .'px;margin: 0 auto;}';
+        
+	    wp_register_style( 'april-inline-customizer', true );
+	    wp_enqueue_style( 'april-inline-customizer' );
+	    wp_add_inline_style( 'april-inline-customizer', $cssstyles );
+
+	endif;
+		return false;
 }
 
 /**
@@ -90,7 +113,7 @@ function april_theme_customizer_css()
 function april_register_theme_customizer_setup($wp_customize)
 {
 	$transport = ( $wp_customize->selective_refresh ? 'postMessage' : 'refresh' );
-    $herodft = get_template_directory_uri() . '/rels/swimmers-wide-default.jpg';
+    $herodft   = get_template_directory_uri() . '/rels/swimmers-wide-default.jpg';
     // Theme font choice section
     $wp_customize->add_section( 'april_general', array(
         'title'       => __( 'April Theme Settings', 'april' ),
@@ -101,7 +124,7 @@ function april_register_theme_customizer_setup($wp_customize)
 	//-----------------Settings and Controls -----------
 	$wp_customize->add_setting( 'april_siteta', array(
 		'type'           => 'theme_mod',
-		'default'          => '#ffffff',
+		'default'          => '#dddddd',
 		'sanitize_callback' => 'sanitize_text_field',
 		'transport'          => 'refresh'
 	) );
@@ -120,16 +143,16 @@ function april_register_theme_customizer_setup($wp_customize)
 		'transport'          => 'refresh'
 	));
 	$wp_customize->add_control( 'april_tposit', array(
-		'label'    => 'Text Alignment for title',
+		'label'    => esc_html__('Text Alignment for title', 'april'),
 		'section'   => 'title_tagline',
 		'settings'   => 'april_tposit',
-		'description' => __( 'Position of title.', 'april'),
+		'description' => esc_html__( 'Position of title.', 'april'),
 		'type'         => 'select',
 		'choices'  => array(
 			'left'  => 'Left',
 			'center' => 'Center',
 			'right'   => 'Right',
-			'relative' => 'Relative',
+			'justify' => 'Justify',
 			)
 	));
 	// relative top and relative left
@@ -140,10 +163,10 @@ function april_register_theme_customizer_setup($wp_customize)
 		'transport'         => 'refresh'
 	));
 	$wp_customize->add_control( 'april_rtposit', array(
-		'label'    => 'Top Adjustment',
+		'label'    => esc_html__('Top Adjustment', 'april'),
 		'section'   => 'title_tagline',
 		'settings'   => 'april_rtposit',
-		'description' => __( 'Alignment must be set to Relative.', 'april'),
+		'description' => esc_html__( 'Alignment must be set to Relative.', 'april'),
 		'type'         => 'number'
 	));
 	$wp_customize->add_setting( 'april_rlposit', array(
@@ -153,10 +176,10 @@ function april_register_theme_customizer_setup($wp_customize)
 		'transport'         => 'refresh'
 	));
 	$wp_customize->add_control( 'april_rlposit', array(
-		'label'   => 'Left Adjustment',
+		'label'   => esc_html__( 'Left Adjustment', 'april'),
 		'section'  => 'title_tagline',
 		'settings'  => 'april_rlposit',
-		'description' => esc_html__( 'Alignment must be set to Relative.', 'april'),
+		'description' => esc_html__( 'Alignment is relative.', 'april'),
 		'type'        => 'number'
 	));
 
@@ -169,7 +192,7 @@ function april_register_theme_customizer_setup($wp_customize)
 		'transport'         => 'refresh'
 	));
 	$wp_customize->add_control( 'april_fontfamily', array(
-		'label'   => 'Font for Content',
+		'label'   => esc_html__( 'Font for Content', 'april'),
 		'section'  => 'april_general',
 		'settings'  => 'april_fontfamily',
 		'description' => esc_html__( 'Choose the font family type.', 'april'),
@@ -208,7 +231,7 @@ function april_register_theme_customizer_setup($wp_customize)
 			'transport'         => 'refresh'
 	));
 	$wp_customize->add_control( 'april_herobksize', array(
-		'label'    => 'Position for Image',
+		'label'    => esc_html__( 'Position for Image', 'april'),
 		'section'   => 'april_general',
 		'settings'   => 'april_herobksize',
 		'description' => esc_html__( 'Position of background image.', 'april'),
@@ -228,7 +251,7 @@ function april_register_theme_customizer_setup($wp_customize)
 		'transport'          => 'refresh'
 	));
 	$wp_customize->add_control( 'april_ctaposit', array(
-		'label'   => 'Position for Buttons',
+		'label'   => esc_html__( 'Position for Buttons', 'april'),
 		'section'  => 'april_general',
 		'settings'  => 'april_ctaposit',
 		'description' => esc_html__( 'Position of CallToAction. (51=right; -12=left) Remember to select a Call To Action widget to display your content with.', 'april'),
@@ -242,7 +265,7 @@ function april_register_theme_customizer_setup($wp_customize)
 		'transport'          => 'refresh'
 	));
 	$wp_customize->add_control( 'april_maybe_banner', array(
-		'label'   => 'Hero from Single Posts',
+		'label'   => esc_html__( 'Hero from Single Posts', 'april'),
 		'section'  => 'april_general',
 		'settings'  => 'april_maybe_banner',
 		'description' => esc_html__( 'Select how you want to show banner on single post pages.', 'april'),
@@ -260,7 +283,7 @@ function april_register_theme_customizer_setup($wp_customize)
 		'transport'          => 'refresh'
 	));
 	$wp_customize->add_control( 'april_maxwidth', array(
-		'label'   => 'Maximum Width of Content',
+		'label'   => esc_html__( 'Maximum Width of Content', 'april'),
 		'section'  => 'april_general',
 		'settings'  => 'april_maxwidth',
 		'description' => esc_html__( 'Sets the width of the aricles for pages and posts. (in pixels)', 'april'),
@@ -290,7 +313,7 @@ function april_custom_header_and_background() {
 		apply_filters(
 			'april_custom_background_args',
 			array(
-				'default-color' => $default_background_color,
+				'default-color' => esc_attr( $default_background_color ),
 			)
 		)
 	);
@@ -305,7 +328,6 @@ function april_custom_header_and_background() {
  * @param WP_Customize_Manager $wp_customize The Customizer object.
  */
 function april_customize_register( $wp_customize ) {
-	//$color_scheme = 'default';
 
 	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
@@ -334,7 +356,7 @@ add_action( 'customize_register', 'april_customize_register', 11 );
 /**
  * Render the site title for the selective refresh partial.
  *
- * @since April 1.2
+ * @since April 1.0
  * @see april_customize_register()
  *
  * @return void
@@ -346,26 +368,11 @@ function april_customize_partial_blogname() {
 /**
  * Render the site tagline for the selective refresh partial.
  *
- * @since April 1.2
+ * @since April 1.0
  * @see april_customize_register()
  *
  * @return void
  */
 function april_customize_partial_blogdescription() {
 	bloginfo( 'description' );
-}
-
-/**
- * Binds JS handlers to make the Customizer preview reload changes asynchronously.
- *
- * @since April 1.0
- */
-function april_customize_preview_js() {
-	wp_enqueue_script( 'april-customize-preview', 
-						get_template_directory_uri() . '/rels/customize-preview.js', 
-						array( 'customize-preview' ), 
-						time(), 
-						true 
-					);
-}
-add_action( 'customize_preview_init', 'april_customize_preview_js' ); 
+} 
