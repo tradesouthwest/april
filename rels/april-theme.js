28b9e45b657af40623de06cee17c32e045f30a58
Larry <tradesouthwest@outlook.com>
2024-05-15 07:10:12 -0700

    Add files via upload

---
+++ rels/april-theme.js
@@ -0,0 +1,29 @@
+/* https://codepen.io/completewebco/pen/Powwxbd */
+/* begin Back to Top button  */
+(function() {
+  'use strict';
+
+  function trackScroll() {
+    var scrolled = window.scrollY;
+    var coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add('back_to_top-show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('back_to_top-show');
+    }
+  }
+
+  function backToTop() {
+    if (window.scrollY > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 0);
+    }
+  }
+
+  var goTopBtn = document.querySelector('.back_to_top');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+})();
